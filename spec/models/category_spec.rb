7d324007d6b11d8323e1091d8817db4bbc54674c
Diego Suarez and Kevin Finley <diego+kevin@>
2017-02-22 11:19:07 -0800

    add specs for models

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe Category do
   it { should have_many :transactions }
   it { should belong_to :user }
+  it { should have_db_column :user_id }
+  it { should have_db_column :name }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Transaction  do
+  it { should have_db_column :user_id }
+  it { should have_db_column :category_id }
+  it { should have_db_column :name }
+  it { should belong_to :user }
+  it { should belong_to :category }
 end
